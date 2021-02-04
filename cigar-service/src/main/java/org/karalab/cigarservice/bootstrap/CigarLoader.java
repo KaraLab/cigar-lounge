5861e1d703811b3d950e7fad6171802e065353a1
KaraTee <kara.nurullah94@gmail.com>
2021-02-04 14:15:31 +0100

    add script to initialize data

--- cigar-service/src/main/java/org/karalab/cigarservice/bootstrap/CigarLoader.java
+++ cigar-service/src/main/java/org/karalab/cigarservice/bootstrap/CigarLoader.java
@@ -6,13 +6,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
-@Component
+//@Component
 public class CigarLoader implements CommandLineRunner {
 
     public static final String CIGAR_1_EAN = "0631234200036";
     public static final String CIGAR_2_EAN = "0631234300019";
     public static final String CIGAR_3_EAN = "0083783375213";
+    public static final UUID CIGAR_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final UUID CIGAR_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+    public static final UUID CIGAR_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     private final CigarRepository cigarRepository;
 
